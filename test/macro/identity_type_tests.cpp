f16b3b81550d5832bd789282b57436db51e3d24f
Aymeric Pellé <aympelle@gmail.com>
2025-01-20 23:46:57 +0100

    v0.1.0:#3: Add preprocessor macro ARBA_CPPX_IDENTITY_TYPE.

---
+++ test/macro/identity_type_tests.cpp
@@ -0,0 +1,17 @@
+#include <arba/cppx/macro/identity_type.hpp>
+
+#include <gtest/gtest.h>
+
+#include <format>
+
+#define ARBA_DEFINE_TYPE(using_type, type) using using_type = ARBA_CPPX_IDENTITY_TYPE(type)
+
+#define DEFINE_TYPE(using_type, type) using using_type = CPPX_IDENTITY_TYPE(type)
+
+TEST(identity_type_tests, identity_type__ok)
+{
+    ARBA_DEFINE_TYPE(container_t, std::vector<int>);
+    static_assert(std::is_same_v<container_t, std::vector<int>>);
+    DEFINE_TYPE(pair_t, (std::pair<int, int>));
+    static_assert(std::is_same_v<pair_t, std::pair<int, int>>);
+}
