03f0efd52f7a096f54be2450e36808068e40d953
Aymeric Pellé <aympelle@gmail.com>
2025-01-20 10:56:00 +0100

    v0.1.0:#3: Add preprocessor macro ARBA_CPPX_STRINGIZE (and ARBA_CPPX_STRINGIZE_CODE).

---
+++ test/macro/stringize_tests.cpp
@@ -0,0 +1,34 @@
+#include <arba/cppx/macro/stringize.hpp>
+
+#include <gtest/gtest.h>
+
+#include <format>
+
+TEST(stringize_tests, stringize__var__ok)
+{
+    int value = 5;
+
+    auto name = ARBA_CPPX_STRINGIZE_CODE(value);
+    static_assert(std::is_same_v<decltype(name), const char*>);
+    std::string str = std::format("{}: {}", name, value);
+    ASSERT_EQ(str, "value: 5");
+
+    auto xname = ARBA_CPPX_STRINGIZE(value);
+    static_assert(std::is_same_v<decltype(xname), const char*>);
+    str = std::format("{}: {}", xname, value);
+    ASSERT_EQ(str, "value: 5");
+}
+
+#define UTVALUE std::size_t(6)
+
+TEST(stringize_tests, stringize__macro__ok)
+{
+    auto str = ARBA_CPPX_STRINGIZE(UTVALUE);
+    auto xstr = ARBA_CPPX_STRINGIZE_CODE(UTVALUE);
+    auto value = UTVALUE;
+    static_assert(std::is_same_v<decltype(str), const char*>);
+    static_assert(std::is_same_v<decltype(xstr), const char*>);
+    static_assert(std::is_same_v<decltype(value), std::size_t>);
+    std::string fstr = std::format("{}: {}", ARBA_CPPX_STRINGIZE_CODE(UTVALUE), ARBA_CPPX_STRINGIZE(UTVALUE));
+    ASSERT_EQ(fstr, "UTVALUE: std::size_t(6)");
+}
