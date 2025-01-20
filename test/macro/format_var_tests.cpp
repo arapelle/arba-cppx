6079071d7711b62ab174d3fb99a42a6046467ebd
Aymeric Pellé <aympelle@gmail.com>
2025-01-20 10:57:18 +0100

    v0.1.0:#3: Add preprocessor macro ARBA_CPPX_FORMAT_VAR.

---
+++ test/macro/format_var_tests.cpp
@@ -0,0 +1,46 @@
+#include <arba/cppx/macro/format_var.hpp>
+
+#include <gtest/gtest.h>
+
+#include <format>
+
+TEST(format_var_tests, format_var__default_var_format_string__var__ok)
+{
+    const int value = 5;
+    std::string fstr = ARBA_CPPX_FORMAT_VAR(value);
+    ASSERT_EQ(fstr, "value = 5");
+    fstr = CPPX_FORMAT_VAR(value);
+    ASSERT_EQ(fstr, "value = 5");
+}
+
+#define UTVALUE std::size_t(6)
+
+TEST(format_var_tests, format_var__default_var_format_string__macro__ok)
+{
+    std::string fstr = ARBA_CPPX_FORMAT_VAR(UTVALUE);
+    ASSERT_EQ(fstr, "UTVALUE = 6");
+    fstr = CPPX_FORMAT_VAR(UTVALUE);
+    ASSERT_EQ(fstr, "UTVALUE = 6");
+}
+
+#undef ARBA_CPPX_VAR_FORMAT_STRING
+#define ARBA_CPPX_VAR_FORMAT_STRING "{}: {}"
+
+TEST(format_var_tests, format_var__custom_var_format_string__var__ok)
+{
+    const int value = 5;
+    std::string fstr = ARBA_CPPX_FORMAT_VAR(value);
+    ASSERT_EQ(fstr, "value: 5");
+    fstr = CPPX_FORMAT_VAR(value);
+    ASSERT_EQ(fstr, "value: 5");
+}
+
+#define UTVALUE std::size_t(6)
+
+TEST(format_var_tests, format_var__custom_var_format_string__macro__ok)
+{
+    std::string fstr = ARBA_CPPX_FORMAT_VAR(UTVALUE);
+    ASSERT_EQ(fstr, "UTVALUE: 6");
+    fstr = CPPX_FORMAT_VAR(UTVALUE);
+    ASSERT_EQ(fstr, "UTVALUE: 6");
+}
